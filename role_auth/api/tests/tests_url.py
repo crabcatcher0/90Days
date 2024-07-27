07d60032f524ccd90b648cfd739e74ff34ac3fa7
CrabCatcher0 <rautprashant103@gmail.com>
2024-07-27 20:41:32 +0545

    user-detail url fixed

--- role_auth/api/tests/tests_url.py
+++ role_auth/api/tests/tests_url.py
@@ -11,16 +11,24 @@ class TestUrls(SimpleTestCase):
         self.assertEqual(resolved_view_func.view_class, UserView)
 
 
-    def test_url_user_is_resolved(self):
+    def test_url_register_is_resolved(self):
         url = reverse('register')
         resolved_view_func = resolve(url).func
         print(resolve(url))
         self.assertEqual(resolved_view_func.view_class, RegisterView)
 
 
-    def test_url_user_is_resolved(self):
+    def test_url_login_is_resolved(self):
         url = reverse('login')
         resolved_view_func = resolve(url).func
         print(resolve(url))
         self.assertEqual(resolved_view_func.view_class, LoginView)
+    
+
+
+    def test_url_profile_id_is_resolved(self):
+        url = reverse('profile-detail', args=[5])
+        resolved_view_func = resolve(url).func
+        print(resolve(url))
+        self.assertEqual(resolved_view_func.view_class, UserView)
     
--- role_auth/api/urls.py
+++ role_auth/api/urls.py
@@ -5,7 +5,7 @@ from api import views
 urlpatterns = [
     path('register', views.RegisterView.as_view(), name='register'),
     path('profile/', views.UserView.as_view(), name='profile'),
-    path('profile/<int:pk>/', views.UserView.as_view()),
+    path('profile/<int:pk>/', views.UserView.as_view(), name='profile-detail'),
     path('login', views.LoginView.as_view(), name='login'),
     path('logout', views.LogoutView.as_view(), name='logout')
 
